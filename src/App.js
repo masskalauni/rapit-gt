635c81b1875d31c3d9b265206fa4a5dab62e0865
mass <premkalauni4777@gmail.com>
2023-12-04 12:28:02 +0545

    form added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+const name = 'mass kalauni ';
+const element = <form>
+<h1>login form</h1>
+<label  htmlFor="username">Username:</label>
+<input type="text" className="username"/>
+<label  htmlFor="password">Password:</label>
+<input type="password" className="password"/>
+<input type="submit" value="SignIn"/>
+</form>;
 
 function App() {
   return (
- <div className="blank">hell </div>
+<>
+<p>{element}</p>
+</>
   );
 }
 
