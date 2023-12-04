c5555e5ddcc31a8b9410991c8beebe076ba98e51
mass <premkalauni4777@gmail.com>
2023-12-04 14:00:34 +0545

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import './App.css';
 
 
-const name = 'mass kalauni ';
-const element = <form>
-<h1>login form</h1>
-<label  htmlFor="username">Username:</label>
-<input type="text" className="username"/>
-<label  htmlFor="password">Password:</label>
-<input type="password" className="password"/>
-<input type="submit" value="SignIn"/>
-</form>;
 
 function App() {
   return (
 <>
-<p>{element}</p>
+<nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">Rapit GT</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">About</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
 </>
   );
 }
